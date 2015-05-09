a5e77c1df497028211b825cbec0c08385730585e
Matt Mazzola <mattmazzola@live.com>
2015-05-09 15:18:19 -0700

    Add error casting

--- app/initializers/errormonitoring.js
+++ app/initializers/errormonitoring.js
@@ -9,12 +9,27 @@ export function initialize(/*container, app */) {
   });
 }
 
-export function logError(err) {
+var convertToError = (errorLikeObject) => {
+    if (Ember.typeOf(errorLikeObject) === 'object') {
+        var message = errorLikeObject.responseText || errorLikeObject.message || errorLikeObject.toString();
+        var status = errorLikeObject.status;
+        var error = new Error(message);
+        if (status) {
+          error.status = status;
+        }
+    }
+    return error;
+};
+
+export function logError(errorLikeObject) {
+  // Ensure that error is an instance of Error
+  let error = convertToError(errorLikeObject);
+
   // // Log in trackJs (prod only)
   // trackJs.track(err);
 
   // Log to console (dev only)
-  Ember.Logger.assert(false, err);
+  Ember.Logger.assert(false, error);
 }
 
 export default {
