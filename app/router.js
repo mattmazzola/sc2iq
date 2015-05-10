ab06f28548f0da72afb808c32974ac1cca4f79d6
Matt Mazzola <mattmazzola@live.com>
2015-05-09 21:48:29 -0700

    Add questions sub routes

--- app/router.js
+++ app/router.js
@@ -6,7 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.route('questions');
+  this.route('questions', function() {
+    this.route('question', { path: '/:question_id'});
+    this.route('loading');
+    this.route('error');
+  });
   this.route('scores');
   this.route('contact');
   this.route('polls');
