3ab54081c20e64312dcbc062dd3f41b93a4c5f1f
Matt Mazzola <mattmazzola@live.com>
2015-05-28 00:06:08 -0700

    Update ember-cli, Setup basic test workflow.

---
+++ app/pods/components/sc-test/component.js
@@ -0,0 +1,55 @@
+import Ember from 'ember';
+let computed = Ember.computed;
+
+export default Ember.Component.extend({
+
+  // Defaults
+  currentQuestionIndex: null,
+  isFinished: false,
+  isStarted: false,
+  questions: null,
+  timeElapsed: null,
+
+  // Single line CP
+
+  // Multiline CP
+  currentQuestion: computed('currentQuestionIndex', function () {
+    return this.questions.objectAt(this.get('currentQuestionIndex'));
+  }),
+
+  // Observers
+  setup: Ember.on('init', function () {
+    this.set('isStarted', false);
+    this.set('isFinished', false);
+    this.set('timeElapsed', new Date());
+    this.set('currentQuestionIndex', 0);
+  }),
+
+  // Actions
+  actions: {
+    quit() {
+      this.setup();
+    },
+
+    restart() {
+      this.setup();
+      this.set('isStarted', true);
+    },
+
+    start() {
+      this.set('isStarted', true);
+    },
+
+    submitAnswer() {
+      let maxIndex = this.get('questions.length') - 1;
+
+      if( maxIndex > this.get('currentQuestionIndex')) {
+        this.incrementProperty('currentQuestionIndex');
+      }
+      else if(maxIndex === this.get('currentQuestionIndex')) {
+        this.set('isFinished', true);
+      }
+    }
+  }
+
+});
--- app/pods/test/route.js
+++ app/pods/test/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.filter('question', {}, () => true)
+    });
+  },
+
+  setupController(controller, model) {
+    controller.setProperties(model);
+    //return this._super.apply(this, arguments);
+  }
 });
