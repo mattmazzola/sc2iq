e3caab22888cd141ec406932a209e9bdb591e36b
Matt Mazzola <mattmazzola@live.com>
2015-07-01 23:39:03 -0700

    Put container inside routes instead of containing each route, to allow backgrounds to fill to the screen width

--- app/pods/contact/controller.js
+++ app/pods/contact/controller.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  message: "",
-
-  actions: {
-    submitForm() {
-      Ember.Logger.log("Comment: ", this.message);
-    }
-  }
 });
