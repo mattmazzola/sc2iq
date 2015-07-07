d6dcbda441684da44ecf83ec3268cdce841dbaf2
Matt Mazzola <mattmazzola@live.com>
2015-07-06 22:44:24 -0700

    Fixes #6 and #7

--- app/pods/components/sc-question-full/component.js
+++ app/pods/components/sc-question-full/component.js
@@ -1,4 +1,44 @@
 import Ember from 'ember';
 
+const {
+  computed
+} = Ember;
+
 export default Ember.Component.extend({
+  isEditMode: false,
+
+  isCurrentUserAdmin: computed('user', function(key, value) {
+    return (this.get('user.role') === 'admin');
+  }),
+
+  answers: computed('question', function (key, value) {
+    return [
+      Ember.Object.create({
+        isCorrect: (this.get('question.a') === 1),
+        text: this.get('question.answer1'),
+      }),
+      Ember.Object.create({
+        isCorrect: (this.get('question.a') === 2),
+        text: this.get('question.answer2'),
+      }),
+      Ember.Object.create({
+        isCorrect: (this.get('question.a') === 3),
+        text: this.get('question.answer3'),
+      }),
+      Ember.Object.create({
+        isCorrect: (this.get('question.a') === 4),
+        text: this.get('question.answer4'),
+      }),
+    ];
+  }),
+
+  actions: {
+    submit() {
+      this.sendAction('action', this.get('question'));
+    },
+
+    toggleProperty(property) {
+      this.set(property, !this.get(property));
+    }
+  }
 });
--- app/pods/questions/question/route.js
+++ app/pods/questions/question/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveQuestion(question) {
+      question.save();
+    }
+  }
 });
