88cbe21afcd2b70ce1702f98c041e7f2c021c11e
Matt Mazzola <mattmazzola@live.com>
2016-02-14 14:30:09 -0800

    Configure project to use pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'sc2iqnew',
+    podModulePrefix: 'sc2iqnew/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
